342cc2fed0eaa4dbe2671dc3813b278abed027c7
Cahroolx <anacarolina.barros009@gmail.com>
2025-09-02 14:08:22 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let nome = "Carol";
+let XP = 8000;
+
+if (XP < 1000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Ferro");
+}else if (XP >= 1001 && XP <= 2000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Bronze");
+}else if (XP >= 2001 && XP <= 5000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Prata");
+}else if (XP >= 5001 && XP <= 7000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Ouro");
+}else if (XP >= 7001 && XP <= 8000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Platina");
+}else if (XP >= 8001 && XP <= 9000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Ascendente");
+}else if (XP >= 9001 && XP <= 10000){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Imortal"); 
+}else if (XP >= 10001){
+    console.log("O heroi de nome " + nome + " possui " + XP + " pontos de experiencia e é classificado como: Radiante");
+}
+
